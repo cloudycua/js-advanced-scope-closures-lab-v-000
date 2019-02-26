d3e11434315e6e1258ed125f3ef2860d48c4abff
Christine Cua <cloudycua@gmail.com>
2019-02-26 02:56:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(blockRange) {
   return function() {
-    if (blockRange > 10) {
-      return true;
+    if (blockRange < 10) {
+      return `within range by ${Math.abs(blockRange - 10)}`;
     } else {
       return `${Math.abs(blockRange - 10)} blocks out of range`;
     };
