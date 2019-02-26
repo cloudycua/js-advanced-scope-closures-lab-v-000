0492e4032cc7bbd46abb4e21efda9eb85d48a8a4
Christine Cua <cloudycua@gmail.com>
2019-02-26 03:15:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(blockRange) {
   return function(start, finish) {
-    rangeDifference = Math.abs(parseInt(start) - parseInt(finish)) - blockRange;
+    distance = Math.abs(parseInt(start) - parseInt(finish));
     if (rangeDifference < 0) {
       return `within range by ${rangeDifference}`;
     } else {
