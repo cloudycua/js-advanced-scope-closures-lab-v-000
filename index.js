f6bb2eef853fb1a30d7cc64f419e0816840653c6
Christine Cua <cloudycua@gmail.com>
2019-02-26 03:16:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(blockRange) {
   return function(start, finish) {
     distance = Math.abs(parseInt(start) - parseInt(finish));
-    if (rangeDifference < 0) {
+    if (blockRange > distance) {
       return `within range by ${rangeDifference}`;
     } else {
       return `${rangeDifference} blocks out of range`;
